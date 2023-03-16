c02590f79804809feb736d60c1939244708ad56e
mrfritzg <mrfritz@gamil.com>
2023-03-16 10:36:47 -0400

    new captians log mern app

---
+++ server.js
@@ -0,0 +1,46 @@
+//require dotENV
+require('dotenv').config();
+
+//Load express
+const express = require('express');
+
+// const connectDB = require('./config/db')
+
+//create our expess app
+const app = express()
+
+const PORT = 8081;
+
+//connect to our database
+// connectDB();
+
+// npm install jsx-view-engine react react-dom
+const {createEngine} = require('jsx-view-engine');
+
+// Load the method-override middleware
+const methodOverride = require('method-override')
+
+// Configure the view engine and look for files ending in jsx
+app.set('view engine', 'jsx')
+
+// Create the engine and accept files ending in jsx
+app.engine('jsx', createEngine())
+
+// a middleware that formats the form data (currently a string that looks like query params) into a object we can use
+app.use(express.urlencoded({ extended: true }))
+
+// hack into our form and give it more HTTP methods (like DELETE and PUT)
+app.use(methodOverride('_method'))
+
+
+// look for static files (like css) in the public folder
+app.use(express.static('public'))
+
+// Setup an "food" index route
+app.get('/', (req, res) => {
+    res.render('New')
+})
+
+app.listen(PORT, () => {
+    console.log('Listening on port '+ PORT)
+})
---
+++ views/New.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function New() {
+    return ( 
+        <div>
+            <h1>New Log</h1>
+            <form action="/logs" method="POST">
+                <label htmlFor="ttl">Title:</label><br />
+                <input type="text" id="ttl" name="title" /><br /><br />
+
+                <label htmlFor="ent">Color:</label><br />
+                <input type="textarea" id="ent" name="entry" /><br /><br />
+
+                <label htmlFor="broken">Ship is Broken</label>
+                <input type="checkbox" id="broken" name="shipIsBroken" /><br /><br />
+
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default New;
